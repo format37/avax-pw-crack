64bf3bfd7bd18fc845996c7553288f6c4e1e8f8a
d0n-w0nton <73367366+d0n-w0nton@users.noreply.github.com>
2023-08-15 21:40:10 -0500

    Update crack_key.py

--- crack_key.py
+++ crack_key.py
@@ -1,7 +1,6 @@
 import hashlib
-from typing import Iterable, Optional
+from typing import Iterable, Optional, List
 
-import web3
 from bip_utils import (
     Bip39MnemonicValidator,
     Bip39SeedGenerator, Bip32Slip10Secp256k1, Bip32KeyIndex, Bech32Encoder,
@@ -15,45 +14,17 @@ class Wallet(object):
         Bip39MnemonicValidator().Validate(mnemonic)
         self.seed_generator = Bip39SeedGenerator(mnemonic)
 
-    def eth_key(self, passphrase: str):
-        seed_bytes = self.seed_generator.Generate(passphrase)
-        master = Bip32Slip10Secp256k1.FromSeed(seed_bytes)
-
-        # m/44'/60'/0'/0/0
-        child = master.ChildKey(Bip32KeyIndex.HardenIndex(44)) \
-            .ChildKey(Bip32KeyIndex.HardenIndex(60)) \
-            .ChildKey(Bip32KeyIndex.HardenIndex(0)) \
-            .ChildKey(0) \
-            .ChildKey(0)
-
-        return child
-
     def avax_key(self, passphrase: str):
         seed_bytes = self.seed_generator.Generate(passphrase)
         master = Bip32Slip10Secp256k1.FromSeed(seed_bytes)
 
         # m/44'/9000'/0'
-        child = master.ChildKey(Bip32KeyIndex.HardenIndex(44)) \
-            .ChildKey(Bip32KeyIndex.HardenIndex(9000)) \
-            .ChildKey(Bip32KeyIndex.HardenIndex(0)) \
-            .ChildKey(0) \
-            .ChildKey(0)
-
+        child = (master
+                 .ChildKey(Bip32KeyIndex.HardenIndex(44))
+                 .ChildKey(Bip32KeyIndex.HardenIndex(9000))
+                 .ChildKey(Bip32KeyIndex.HardenIndex(0))
+                 .ChildKey(0))
         return child
-
-
-def child_to_eth_address(child) -> str:
-    eth_account = web3.Account.from_key(child.PrivateKey().Raw().ToHex())
-    return eth_account.address
-
-
-def guess_eth_address(inputs: Iterable[str], wallet: Wallet, target_address: str) -> Optional[str]:
-    for test_passphrase in inputs:
-        child = wallet.eth_key(test_passphrase)
-        computed_address = child_to_eth_address(child)
-
-        if computed_address == target_address:
-            return test_passphrase
 
 
 def child_to_avaxp_address(child) -> str:
@@ -67,33 +38,44 @@ def child_to_avaxp_address(child) -> str:
     return f'P-{b32_encoded}'
 
 
-def guess_avaxp_address(inputs: Iterable[str], wallet: Wallet, target_address: str) -> Optional[str]:
-    for test_passphrase in inputs:
-        child = wallet.avax_key(test_passphrase)
-        computed_address = child_to_avaxp_address(child)
+def generate_10_avax_addresses(passphrase: str, wallet: Wallet) -> List[str]:
+    """Generate 10 AVAX addresses for the given passphrase."""
+    addresses = []
+    base_child = wallet.avax_key(passphrase)
+    for i in range(10):
+        child = base_child.ChildKey(i)  # Iterate over the last index to generate 10 keys
+        addresses.append(child_to_avaxp_address(child))
+    return addresses
 
-        if computed_address == target_address:
-            return test_passphrase
+
+def guess_avaxp_address(inputs: Iterable[str], wallet: Wallet, target_addresses: list) -> Optional[str]:
+    for test_passphrase in inputs:
+        computed_addresses = generate_10_avax_addresses(test_passphrase, wallet)
+
+        # Check for any matches between the computed addresses and target addresses
+        for address in computed_addresses:
+            if address in target_addresses:
+                return test_passphrase
+    return None
 
 
 if __name__ == "__main__":
-    mnemonic = "Forum cloud autumn carry build dove image series like general tribe glove alone saddle eager verb width envelope absurd need wool faith garlic quit"
-    # myfakephrase is the actual password.
-    best_guess = "MONEY"
-    expected_eth_address = '0xdc22f43BEFeb27E5B8185Ae6d336b41295998526'
-    expected_avaxp_address = 'P-avax12tua7tqjntsgyz4dgjvqggy0pj4kst2zwmy2u9'
+    mnemonic = "insert mnemonic here"
+    best_guess = "guess"
+    expected_avaxp_addresses = [
+        'P-avax1',
+        'P-avax1',
+	    'P-avax1',
+	    'P-avax1',
+	    'P-avax1',
+	    'P-avax1',
+	    'P-avax1',
+	    'P-avax1',
+	    'P-avax1',
+	    'P-avax1',
+    ]
 
     wallet = Wallet(mnemonic)
-
-    # Can take this and import into https://wallet.avax.network to get the corresponding
-    # c/p chain addresses with the 25th word.
-    # Can also use https://iancoleman.io/bip39/ but it only supports eth and not avax.
-    # print(wallet.eth_key('myfakephrase').PrivateKey().Raw().ToHex())
-    # print(wallet.avax_key('myfakephrase').PrivateKey().Raw().ToHex())
-
-    # If false, uses uppercase instead of lowercase letters
-    lowercase = True
+    lowercase = False
     typos = Typos(best_guess, max_edit_distance=2, lowercase=lowercase)
-
-    # print(guess_eth_address(typos, wallet, expected_eth_address))
-    print(guess_avaxp_address(typos, wallet, expected_avaxp_address))
+    print(guess_avaxp_address(typos, wallet, expected_avaxp_addresses))
