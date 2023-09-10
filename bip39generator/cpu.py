4ef93a3d7fe4970ebd2e25ca576d360566edff0c
Alex <format37@gmail.com>
2023-09-10 22:00:49 +0400

    modified:   cpu.py

--- bip39generator/cpu.py
+++ bip39generator/cpu.py
@@ -1,4 +1,11 @@
+import bip_utils
 from bip_utils import Bip39SeedGenerator
+# import pkg_resources
+import os
+import inspect
+
+# print(pkg_resources.get_distribution("bip_utils").version)
+print(os.path.dirname(inspect.getfile(bip_utils)))
 
 mnemonic = 'sell stereo useless course suffer tribe jazz monster fresh excess wire again father film sudden pelican always room attack rubber pelican trash alone cancel'
 passphrase = 'TESTPHRASG'
