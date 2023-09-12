6c2f17de116061bbed22df583d59d40b4c533c27
Alex <format37@gmail.com>
2023-09-13 00:50:54 +0400

    modified:   cpu.py

--- bip39generator/cpu.py
+++ bip39generator/cpu.py
@@ -4,6 +4,10 @@ from bip_utils import Bip39SeedGenerator
 import os
 import inspect
 
+# Function to add hyphens to the hex string after every 4 bytes (8 characters)
+def add_hyphens_to_hex(hex_str):
+    return '-'.join([hex_str[i:i+8] for i in range(0, len(hex_str), 8)])
+
 # print(pkg_resources.get_distribution("bip_utils").version)
 print(os.path.dirname(inspect.getfile(bip_utils)))
 
@@ -17,3 +21,6 @@ print('seed_bytes:', seed_bytes)
 # Convert from bytes to number
 seed_num = int.from_bytes(seed_bytes, byteorder='big')
 print('seed_num:', seed_num)
+hex_result = seed_bytes.hex()
+hex_result_with_hyphens = add_hyphens_to_hex(hex_result)
+print('result:', hex_result_with_hyphens)
