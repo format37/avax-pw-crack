42faa63241a752ed3a5cd4301b7863abe3ac078f
Alex <format37@gmail.com>
2023-11-04 13:52:34 +0400

    new file:   build.sh 	new file:   include/bn.h 	new file:   include/ec.h 	new file:   include/obj_mac.h 	new file:   public_openssl_c_local.c 	new file:   run.sh

---
+++ public/openssl_c_local/build.sh
@@ -0,0 +1,4 @@
+# clear
+rm -f program
+# gcc program.c -o program -lcrypto
+gcc -I/home/alex/projects/avax-pw-crack/public/openssl_c_local/include public_openssl_c_local.c -o program -std=c99 -lcrypto
