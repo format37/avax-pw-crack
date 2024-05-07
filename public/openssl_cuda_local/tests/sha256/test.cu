0e7fb3f1fb4e47079d1c7e5a2d5e13d477137b2e
Alex <format37@gmail.com>
2024-05-07 13:51:02 +0400

    modified:   ../../include/bignum.h 	new file:   build.sh 	new file:   run.sh 	new file:   test.cu

---
+++ public/openssl_cuda_local/tests/sha256/test.cu
@@ -0,0 +1,20 @@
+#include "bignum.h" // Big number arithmetic
+
+__global__ void test() {
+    // Convert the mnemonic and passphrase to byte arrays (or use them as-is if you can)
+    uint8_t *m_mnemonic = (unsigned char *)"sell stereo useless course suffer tribe jazz monster fresh excess wire again father film sudden pelican always room attack rubber pelican trash alone cancel";
+    uint8_t *salt = (unsigned char *)"mnemonicTESTPHRASE";
+    compute_sha256((uint8_t *) m_mnemonic, my_strlen((const char*) m_mnemonic));
+}
+
+// Main function
+int main() {
+    test<<<1, 1>>>();
+    cudaError_t err = cudaGetLastError();
+    if (err != cudaSuccess) {
+        printf("Error: %s\n", cudaGetErrorString(err));
+        return -1;
+    }
+    cudaDeviceSynchronize();
+    return 0;
+}
