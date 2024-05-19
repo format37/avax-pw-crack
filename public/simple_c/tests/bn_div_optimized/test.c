918682299775a7f62172ff953caf7877c648af91
Alex <format37@gmail.com>
2024-05-19 15:54:33 +0400

    modified:   test.c

--- public/simple_c/tests/bn_div_optimized/test.c
+++ public/simple_c/tests/bn_div_optimized/test.c
@@ -2,9 +2,18 @@
 
 #define BN_ULONG unsigned long long
 #define BN_ULONG_NUM_BITS 64
+#define BN_ULONG_NUM_SYMBOLS BN_ULONG_NUM_BITS/8
 #define WORDS 2
 
-void bn_print(const char *prefix, const BN_ULONG *a)
+void reverse_order(BN_ULONG *test_values_a) {
+    for (size_t j = 0; j < WORDS / 2; j++) {
+        BN_ULONG temp_a = test_values_a[j];
+        test_values_a[j] = test_values_a[WORDS - 1 - j];
+        test_values_a[WORDS - 1 - j] = temp_a;
+    }
+}
+
+void bn_print_reversed_words(const char *prefix, const BN_ULONG *a)
 {
     printf("%s", prefix);
     for (int i = 0; i < WORDS; ++i) {
@@ -13,7 +22,16 @@ void bn_print(const char *prefix, const BN_ULONG *a)
     printf("\n");
 }
 
-int find_top(const BN_ULONG *a, int top)
+void bn_print(const char *prefix, const BN_ULONG *a)
+{
+    printf("%s", prefix);
+    for (int i = WORDS - 1; i >= 0; --i) {
+        printf("%016llX ", a[i]);
+    }
+    printf("\n");
+}
+
+unsigned char find_top(const BN_ULONG *a, int top)
 {
     while (top > 0 && a[top - 1] == 0) {
         --top;
@@ -37,30 +55,14 @@ int get_msb_bit(BN_ULONG *n) {
     return msb_bit - 1;
 }
 
-int count_significant_symbols(const BN_ULONG *a)
+int top_word_significant_symbols(const BN_ULONG *a, const unsigned char top)
 {
-    int count = 0;
-    int i = WORDS - 1;
-
-    // Skip leading zero words
-    while (i >= 0 && a[i] == 0) {
-        i--;
-    }
-
-    if (i < 0) {
-        return 0; // All zeros
-    }
-
-    // Count significant symbols in the top non-zero word
-    BN_ULONG word = a[i];
-    while (word != 0) {
-        word >>= 4;
+    char count = 0;
+    for (int i = 0; i < BN_ULONG_NUM_SYMBOLS; i++) {
+        BN_ULONG word = a[top - 1];
+        if ((word >> 4 * i) == 0) break;
         count++;
     }
-
-    // Add the number of symbols in the remaining non-zero words
-    count += i * sizeof(BN_ULONG) * 2;
-
     return count;
 }
 
@@ -69,8 +71,8 @@ int bn_div(const BN_ULONG *dividend, const BN_ULONG *divisor, BN_ULONG *quotient
     printf("++ bn_div ++\n");
     printf("dividend top: %d\n", find_top(dividend, WORDS));
     printf("divisor top: %d\n", find_top(divisor, WORDS));
-    printf("dividend count_significant_symbols: %d\n", count_significant_symbols(dividend));
-    printf("divisor count_significant_symbols: %d\n", count_significant_symbols(divisor));
+    printf("dividend count_significant_symbols: %d\n", top_word_significant_symbols(dividend, find_top(dividend, WORDS)));
+    printf("divisor count_significant_symbols: %d\n", top_word_significant_symbols(divisor, find_top(divisor, WORDS)));
     quotient[0] = 0x3;
     remainder[0] = 0x4;
 
@@ -83,8 +85,11 @@ int main()
     BN_ULONG dividend[WORDS] = {0, 0xb0c89}; //724105
     BN_ULONG divisor[WORDS] = {0, 0xd97}; //3479
 
+    reverse_order(dividend);
+    reverse_order(divisor);
+
     bn_print("dividend = ", dividend);
-    bn_print("dividend = ", divisor);
+    bn_print("divisor = ", divisor);
 
     BN_ULONG quotient[WORDS];
     BN_ULONG remainder[WORDS];
