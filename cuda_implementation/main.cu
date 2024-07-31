755817e17c0daecd117dbca0141d57cd07ce2ed4
Alex <format37@gmail.com>
2024-07-31 19:52:00 +0400

    modified:   cuda_implementation/main.cu

--- cuda_implementation/main.cu
+++ cuda_implementation/main.cu
@@ -7,6 +7,64 @@
 
 #define TEST_BIGNUM_WORDS 4
 
+__device__ void print_as_hex_char_tmp(unsigned char *data, int len) {
+    for (int i = 0; i < len; i++) {
+        printf("%02x", data[i]);
+    }
+    printf("\n");
+}
+
+// BIP32 ++
+typedef struct {
+    unsigned char master_private_key[32];
+    unsigned char chain_code[32];
+} BIP32Info;
+
+__device__ void my_cuda_memcpy_unsigned_char_b(uint8_t *dst, const uint8_t *src, unsigned int n) {
+    for (unsigned int i = 0; i < n; ++i) {
+        dst[i] = src[i];
+    }
+}
+
+__device__ BIP32Info bip32_from_seed_kernel(const uint8_t *seed, uint32_t seed_len) {
+    printf("++ bip32_from_seed_kernel ++\n");
+    printf(">> seed: ");
+    print_as_hex(seed, seed_len);
+    printf(">> seed_len: %d\n", seed_len);
+
+    BIP32Info info;
+	// Initialize HMAC_SHA512_CTX
+    HMAC_SHA512_CTX hmac;
+    
+    // Compute HMAC-SHA512 with "Bitcoin seed" as the key
+    hmac_sha512_init(&hmac, (const uint8_t *)"Bitcoin seed", 12);
+    hmac_sha512_update(&hmac, seed, seed_len);
+
+    // Print hmac
+    // printf("# hmac: ");
+    
+    unsigned char hash[64];
+    // clear hash
+    for (int i = 0; i < 64; ++i) {
+        hash[i] = 0;
+    }
+    hmac_sha512_final(&hmac, hash);
+
+    // Print hash
+    printf("# hash: ");
+    print_as_hex(hash, 64);
+    
+    // Copy the first 32 bytes to master_private_key and the next 32 bytes to chain_code
+    //my_cuda_memcpy_unsigned_char(info->master_private_key, hash, 32);
+    //my_cuda_memcpy_unsigned_char(info->chain_code, hash + 32, 32);
+	my_cuda_memcpy_unsigned_char_b(info.master_private_key, hash, 32);
+	my_cuda_memcpy_unsigned_char_b(info.chain_code, hash + 32, 32);
+
+    printf("-- bip32_from_seed_kernel --\n");
+	return info;
+}
+// BIP32 --
+
 __device__ void reverse_order(BIGNUM *test_values_a) {
     for (size_t j = 0; j < TEST_BIGNUM_WORDS / 2; j++) {
         BN_ULONG temp_a = test_values_a->d[j];
@@ -24,10 +82,10 @@ __global__ void search_kernel() {
     print_as_hex(m_mnemonic, 156);
 
     uint8_t *salt = (unsigned char *)"mnemonicTESTPHRASE";
-    unsigned char derived_key[64];  // This will hold the generated seed
-    // Initialize derived_key to zeros
+    unsigned char bip39seed[64];  // This will hold the generated seed
+    // Initialize bip39seed to zeros
     for (int i = 0; i < 64; ++i) {
-        derived_key[i] = 0;
+        bip39seed[i] = 0;
     }
 
     // Call pbkdf2_hmac to perform the key derivation
@@ -38,12 +96,20 @@ __global__ void search_kernel() {
         my_strlen((const char*) salt),
 	    2048, 
         64,
-        derived_key
+        bip39seed
         );
-    printf("Cuda derived_key: ");
-    print_as_hex(derived_key, 64);  
+    printf("bip39seed: ");
+    print_as_hex(bip39seed, 64);
 
-    printf("-- search_kernel --\n");    
+    // +++ Bip32FromSeed +++
+    BIP32Info master_key = bip32_from_seed_kernel(bip39seed, 64);
+    printf("\nMaster Chain Code: ");
+    print_as_hex_char_tmp(master_key.chain_code, 32);
+    printf("\nMaster Private Key: ");
+    print_as_hex_char_tmp(master_key.master_private_key, 32);
+    // --- Bip32FromSeed ---
+
+    printf("\n-- search_kernel --\n");    
 }
 
 int main() {
