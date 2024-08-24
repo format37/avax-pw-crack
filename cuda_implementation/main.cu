04a829634af47024f896ea86859a6e0a54b6ddf1
Alex <format37@gmail.com>
2024-08-24 20:08:50 +0400

    modified:   cuda_implementation/main.cu

--- cuda_implementation/main.cu
+++ cuda_implementation/main.cu
@@ -10,18 +10,15 @@ __global__ void search_kernel() {
     uint8_t *mnemonic = (unsigned char *)"sell stereo useless course suffer tribe jazz monster fresh excess wire again father film sudden pelican always room attack rubber pelican trash alone cancel";
     const char *passphrase = "TESTPHRASE";
     P_CHAIN_ADDRESS_STRUCT p_chain_address = restore_p_chain_address(mnemonic, passphrase);
-    // printf("[%d] Restored P-chain address: %s\n", thread_id, p_chain_address.data);
+    printf("[%d] Restored P-chain address: %s\n", thread_id, p_chain_address.data);
 }
 
 int main() {
     
-    // const int THREADS_PER_BLOCK = 256; // A good balance between occupancy and flexibility. Max threads per block: 1024
-    // const int NUM_BLOCKS = 128; // One block per SM OK
+    const int THREADS_PER_BLOCK = 256; // A good balance between occupancy and flexibility. Max threads per block: 1024
+    const int NUM_BLOCKS = 128; // One block per SM OK
 
-    
-    const int THREADS_PER_BLOCK = 256; // 32 threads per warp. 8 warps per block.
-    const int NUM_BLOCKS = 512; // 64 warps.
-    // 256 * 512 = 131072 which is max threads per SM.
+    // More threads lead to mistakes
 
     // Launch kernel
     search_kernel<<<NUM_BLOCKS, THREADS_PER_BLOCK>>>();
